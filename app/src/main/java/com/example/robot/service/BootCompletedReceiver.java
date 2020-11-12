74ce6940a4c6290534ff46a7e64d7a8bd32b4c29
zhangdan <zhangdan@realthon.com>
2020-11-12 09:22:37 +0800

    开机自启

--- app/src/main/java/com/example/robot/service/BootCompletedReceiver.java
+++ app/src/main/java/com/example/robot/service/BootCompletedReceiver.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.example.robot.activity.MapManagerActivity;
+
 public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("BootCompletedReceiver", "开机启动");
-        //Intent intent1 = new Intent(context, MapManagerActivity.class);
-        //context.startActivity(intent1);
+        Intent intent1 = new Intent(context, MapManagerActivity.class);
+        context.startActivity(intent1);
     }
 }
